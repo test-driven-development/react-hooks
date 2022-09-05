68be76b22f4c22552f19f4877da29709a6df3ca0
wpannell <wil.pannell@gmail.com>
2022-09-05 03:29:07 -0700

    initialize item from local storage if it exists

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,7 +6,9 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') ?? initialName,
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
